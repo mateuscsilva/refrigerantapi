348f88a7361f75a585101c556cf002ab4af8fd18
mateus.carvalho <mateuscsilva.1@gmail.com>
2021-07-26 11:35:03 -0300

    Adiciona funcionalidade na controller
    Adiciona funcionalidades na controller relacionadas as operacoes
    inseridas no service
    
    Insere mapeamentos do tipo get, post, delete e patch na REST API

--- src/main/java/one/digitalinnovation/refrigerantapi/controller/SodaController.java
+++ src/main/java/one/digitalinnovation/refrigerantapi/controller/SodaController.java
@@ -1,5 +1,51 @@
 package one.digitalinnovation.refrigerantapi.controller;
 
+import lombok.AllArgsConstructor;
+import one.digitalinnovation.refrigerantapi.dto.QuantityDTO;
+import one.digitalinnovation.refrigerantapi.dto.SodaDTO;
+import one.digitalinnovation.refrigerantapi.exception.SodaAlreadyRegisteredException;
+import one.digitalinnovation.refrigerantapi.exception.SodaNotFoundException;
+import one.digitalinnovation.refrigerantapi.exception.SodaStockExceededException;
+import one.digitalinnovation.refrigerantapi.service.SodaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/sodas")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class SodaController {
+
+    private final SodaService sodaService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public SodaDTO createSoda(@RequestBody @Valid SodaDTO sodaDTO) throws SodaAlreadyRegisteredException {
+        return sodaService.createSoda(sodaDTO);
+    }
+
+    @GetMapping("/{name}")
+    public SodaDTO findByName(@PathVariable String name) throws SodaNotFoundException {
+        return sodaService.findByName(name);
+    }
+
+    @GetMapping
+    public List<SodaDTO> listSodas(){ return sodaService.listAlL(); }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(Long id) throws SodaNotFoundException {
+        sodaService.deleteById(id);
+    }
+
+    @PatchMapping("/{id}/increment")
+    public SodaDTO increment(@PathVariable Long id, @RequestBody @Valid QuantityDTO quantityDTO) throws SodaNotFoundException,
+            SodaStockExceededException {
+        return sodaService.increment(id, quantityDTO.getQuantity());
+    }
+
+
 }
