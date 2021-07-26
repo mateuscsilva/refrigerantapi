4b8f2f324bed9f0d4dac74722e6f5f8002c124a6
mateus.carvalho <mateuscsilva.1@gmail.com>
2021-07-26 09:17:48 -0300

    first commit with project configuration

---
+++ src/main/java/one/digitalinnovation/refrigerantapi/RefrigerantapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.refrigerantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefrigerantapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefrigerantapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/refrigerantapi/RefrigerantapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.refrigerantapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefrigerantapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
